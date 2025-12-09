8a02f9de3fd05d64635adc4e6f66d966782b112e
MariamBouhassoune <m.bouhassoune@myskolae.fr>
2025-12-10 00:41:15 +0100

    add exercise type validation schema

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
 export * from "./badgeModel";
 export * from "./gymModel";
 export * from "./userModel";
+export * from "./exerciseTypeModel";
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,3 +1,4 @@
 export * from "./userSchema";
 export * from "./gymSchema";
 export * from "./badgeSchema";
+export * from "./exerciseTypeSchema";
